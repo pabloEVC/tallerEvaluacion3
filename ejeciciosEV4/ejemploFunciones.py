01e3720b85cbb2588b93650e65ed4d0c5ca77e67
pabloEVC <p.valderramac@duocuc.cl>
2026-05-30 11:33:24 -0400

    import funciones

--- ejeciciosEV4/ejemploFunciones.py
+++ ejeciciosEV4/ejemploFunciones.py
@@ -1,10 +1,15 @@
 #funciones
-def sumanumeros():
-    """Función que suma dos números ingresados por el usuario."""
+import funciones
+def funciones():
+    """Función que solicita al usuario dos números y devuelve su suma."""
     num1=int(input("ingrese numero 1: "))
-    num2=int(input("ingrese numero 2: "))
 
+
+#funciones con parametros
+def sumanumeros(num1,num2):
+    """Función que suma dos números dados como parámetros."""
     suma=num1+num2
     return suma
-
-print(sumanumeros())
+numero1=int(input("ingrese numero 1: "))
+numero2=int(input("ingrese numero 2: "))
+print(sumanumeros(numero1,numero2))
---
+++ ejeciciosEV4/funciones.py
@@ -0,0 +1,8 @@
+def sumanumeros():
+    """Función que suma dos números ingresados por el usuario."""
+    num1=int(input("ingrese numero 1: "))
+    num2=int(input("ingrese numero 2: "))
+
+    suma=num1+num2
+    return suma
+print(sumanumeros())
